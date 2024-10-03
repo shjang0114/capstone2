15d7d993ae6b91aead99e3703a979b6cc8ae882d
shjang0114 <eprpenguin1844@gmail.com>
2024-10-04 01:01:39 +0900

    Organize code

--- teamProj_final/src/main/java/com/capstone/teamProj_10/apiTest/question/QuestionController.java
+++ teamProj_final/src/main/java/com/capstone/teamProj_10/apiTest/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.capstone.teamProj_10.apiTest.question;
 //import com.mysite.sbb.answer.AnswerForm;
 //import com.mysite.sbb.user.SiteUser;
 //import com.mysite.sbb.user.UserService;
+
 import com.capstone.teamProj_10.apiTest.answer.AnswerForm;
 import com.capstone.teamProj_10.apiTest.user.SiteUser;
 import com.capstone.teamProj_10.apiTest.user.UserService;
@@ -24,74 +25,74 @@ import java.security.Principal;
 @Controller
 public class QuestionController {
 
-	private final QuestionService questionService;
-	private final UserService userService;
+    private final QuestionService questionService;
+    private final UserService userService;
 
-	@RequestMapping("/question/list")
-	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
-			@RequestParam(value = "kw", defaultValue = "") String kw) {
-		Page<Question> paging = this.questionService.getList(page, kw);
-		model.addAttribute("paging", paging);
-		model.addAttribute("kw", kw);
-		return "question_list";
-	}
+    @RequestMapping("/question/list")
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+                       @RequestParam(value = "kw", defaultValue = "") String kw) {
+        Page<Question> paging = this.questionService.getList(page, kw);
+        model.addAttribute("paging", paging);
+        model.addAttribute("kw", kw);
+        return "question_list";
+    }
 
-	@GetMapping(value = "/question/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
-		Question question = this.questionService.getQuestion(id);
-		model.addAttribute("question", question);
-		return "question_detail";
-	}
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("question/create")
-	public String questionCreate(QuestionForm questionForm) {
-		return "question_form";
-	}
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("question/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@PostMapping("question/create")
-	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
-		if (bindingResult.hasErrors()) {
-			return "question_form";
-		}
-		SiteUser siteUser = this.userService.getUser(principal.getName());
-		this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
-		return "redirect:/question/list";
-	}
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("question/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
+        return "redirect:/question/list";
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@RequestMapping("question/modify/{id}")
-	public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal,
-			@PathVariable("id") Integer id) {
-		if (bindingResult.hasErrors()) {
-			return "question_form";
-		}
-		Question question = this.questionService.getQuestion(id);
-		if (!question.getAuthor().getUsername().equals(principal.getName())) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
-		}
-		this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
-		return String.format("redirect:/question/detail/%s", id);
-	}
-	
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("question/delete/{id}")
-	public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
-		Question question = this.questionService.getQuestion(id);
-		if (!question.getAuthor().getUsername().equals(principal.getName())) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
-		}
-		this.questionService.delete(question);
-		return "redirect:/";
-	}
-	
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("question/vote/{id}")
-	public String questionVote(Principal principal, @PathVariable("id") Integer id) {
-		Question question = this.questionService.getQuestion(id);
-		SiteUser siteUser = this.userService.getUser(principal.getName());
-		this.questionService.vote(question, siteUser);
-		return String.format("redirect:/question/detail/%s", id);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping("question/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal,
+                                 @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("question/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("question/vote/{id}")
+    public String questionVote(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.questionService.vote(question, siteUser);
+        return String.format("redirect:/question/detail/%s", id);
+    }
 }
--- teamProj_final/src/main/java/com/capstone/teamProj_10/apiTest/question/QuestionForm.java
+++ teamProj_final/src/main/java/com/capstone/teamProj_10/apiTest/question/QuestionForm.java
@@ -8,10 +8,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuestionForm {
-	@NotEmpty(message = "제목은 필수항목 입니다.")
-	@Size(max = 200)
-	private String subject;
-	
+    @NotEmpty(message = "제목은 필수항목 입니다.")
+    @Size(max = 200)
+    private String subject;
+
 //	@NotEmpty(message = "카테고리는 필수항목 입니다.")
 //	@Size(max = 50)
 //	private String category;
@@ -20,6 +20,6 @@ public class QuestionForm {
 //	@Size(max = 50)
 //	private String price;
 
-	@NotEmpty(message = "내용은 필수항목 입니다.")
-	private String content;
+    @NotEmpty(message = "내용은 필수항목 입니다.")
+    private String content;
 }
