f581c54db42752bb951a850291a615a4873b2c2a
shjang0114 <eprpenguin1844@gmail.com>
2024-10-21 20:40:02 +0900

    Organize code

--- teamProj_final/src/main/java/com/capstone/teamProj_10/apiTest/question/QuestionForm.java
+++ teamProj_final/src/main/java/com/capstone/teamProj_10/apiTest/question/QuestionForm.java
@@ -8,18 +8,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuestionForm {
-    @NotEmpty(message = "제목은 필수항목 입니다.")
+    @NotEmpty(message = "제목은 필수 항목입니다.")
     @Size(max = 200)
     private String subject;
 
-//	@NotEmpty(message = "카테고리는 필수항목 입니다.")
+//	@NotEmpty(message = "카테고리는 필수 항목입니다.")
 //	@Size(max = 50)
 //	private String category;
 //
-//	@NotEmpty(message = "가격은 필수항목 입니다.")
+//	@NotEmpty(message = "가격은 필수 항목입니다.")
 //	@Size(max = 50)
 //	private String price;
 
-    @NotEmpty(message = "내용은 필수항목 입니다.")
+    @NotEmpty(message = "내용은 필수 항목입니다.")
     private String content;
 }
